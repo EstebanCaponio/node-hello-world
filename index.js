2915752f9ed23a1b0df32007affa037c9b915ea8
EstebanCaponio <estebancaponio@gmail.com>
2025-03-25 16:17:44 +0100

    feat: finish bonus

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello Boolean');
+// console.log('Hello Boolean');
+// console.log(process.argv);
+
+if (process.argv.length > 2) {
+
+    let newString = '';
+
+    for (let i = 2; i < process.argv.length; i++) {
+
+        newString+= ` ${process.argv[i]}`;
+    }
+
+    console.log(newString);
+
+} else console.log('Hello Boolean');
